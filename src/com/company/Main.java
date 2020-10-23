3ff8c22759b122034dc77a9079e9aa132ab02c18
Roman Volobuev <BroChilling@yandex.ru>
2020-10-24 00:45:42 +0700

    change branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println("Git, privet");
+
+        System.out.println("Git, privet, ");
+        System.out.println("qqqq");
 	// write your code here
     }
 }
